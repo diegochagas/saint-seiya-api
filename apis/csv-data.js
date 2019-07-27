3288a312c4fc0dcf45d1dcdc9cb6425206e86c62
diegochagas <drochachagas@gmail.com>
2019-07-27 09:51:04 -0300

    Update data

---
+++ apis/csv-data.js
@@ -0,0 +1,475 @@
+module.exports = `
+class
+Saints
+Saints
+Saint of Athena
+Saints
+Jewel
+Saint of Athena
+Saints(former) | Pallasites(current) | Saturnians
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saint of Athena
+Saint of Athena
+null
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Knight of the Round Table (Formerly) | Gladiator (Formerly) | Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Gold Saints | Resurrected Saint
+Saints
+Saints
+Saints
+Saints | Saintias
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+null
+Saints
+Bronze Saint of Athena | Saintia of Athena
+Saints
+Saints
+Saints(former) | Specters(current)
+Saints
+Saints
+Saints
+Saints | God Warriors
+Marinas
+Saint of Athena
+Saints
+Saints | Saintias
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints | Saintias
+Saints | Saintias
+Saints
+Saints
+Saints
+Saints
+Marinas | Saints
+Saints(former) | Pallasites(later)
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saint of Athena
+Saints
+Saints
+Saint of Athena
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints | Dryades
+Saints
+Saints
+Saints
+Saints
+Saint
+Saint Of Athena
+Saints
+Saints
+Saints
+Saints
+Saint of Athena
+Saint
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints |  | Pope
+Saints
+Saints
+Saints
+Saints
+Saints
+Martians | Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints | Saintias
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints (formerly) | Specters
+Saints
+Saints
+Saints
+null
+null
+Saint of Athena
+Saints
+null
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saint
+Saints
+Saints
+Saints
+Saints
+Saints
+Saint
+Saints
+Saints
+Saints
+Saints
+Giant
+Giant
+Giant
+Giant
+Giant
+Giant
+Giant
+Giant
+Giant
+Gigas
+Gigas
+Gigas
+Gigas
+Gigas
+Gigas
+Gigas
+Gigas
+Gigas
+Titan
+Titan
+Titan
+Titan
+Titans
+Titans
+Titan
+Titan
+Titans
+Titan
+Titan
+Titan
+Titan
+Titan
+Titan
+Fairy
+Dryades
+Dryades
+Dryades
+Dryades
+Dryades
+Dryades
+Dryades
+Dryades
+Dryades
+Taonias
+Taonias
+Taonias
+Taonias
+Taonias
+Pythias
+Berserkers
+Berserkers
+Berserkers
+Berserkers
+Berserkers
+Deities
+Deities
+Deities
+Deities
+Jewels
+Jewels
+Jewels
+Jewels
+Jewels
+Jaguar
+Satellites
+Satellites
+null
+Martians
+Martian
+Martian
+Martians
+Martian | Saints
+Martian
+Martian
+Martian
+Martian
+Martian
+Martian
+Martian
+Pallasites | Saturnians (formerly)
+Pallasites | Saturnians
+Pallasites | Saturnians
+Pallasites | Saturnians
+Pallasites
+Pallasites | Saturnians
+Pallasites | Saturnians
+Pallasites | Saturnians
+Pallasites | Saturnians
+Pallasites
+Pallasites
+Pallasites
+Pallasites
+Pallasites
+Pallasites
+Pallasites
+Pallasites
+Pallasites
+Pallasites | Saturnians
+null
+null
+null
+null
+null
+Faceless
+Faceless
+Faceless
+Faceless
+Knight of the Round Table (Formerly) | Gladiator (Formerly) | Saints
+Gladiators
+Gladiators
+Gladiator
+Gladiators
+Gladiators
+Blue Warrior | God Warrior
+Blue Warriors
+null
+Blue Warriors
+Civilian
+God Warriors
+Warrior
+Warrior
+Warrior
+null
+Warrior
+Warrior
+Warrior
+Warrior
+Warrior
+Warrior
+Warrior
+Warrior
+null
+God Warriors
+God Warrior of Odin
+God Warrior of Odin
+God Warrior of Odin
+God Warrior of Odin
+God Warrior of Odin
+God Warrior of Odin
+Mariner of Poseidon
+Marinas
+Marinas
+Marinas
+Marinas
+Mariner General of Poseidon
+Marinas
+Marinas
+Specters | Deities
+Specters
+Specters
+Specter of Hades
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+Specters
+null
+Gladiator | Former Saint Apprentice
+null
+null
+null
+Angels
+null
+Angels
+Heaven Warrior of Olympus
+Angel
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Demi-God
+Demi-God
+Deities
+Demi-god
+Deities
+Deities
+Deities
+Primordial deity
+Primordial deity
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Deities
+Jaguar | Deities
+Deities
+Saints
+Automaton
+Witch
+null
+null
+Saints
+Soldiers
+null
+Saints
+Saints
+Saints
+Saints
+null
+Saints
+null
+Saints
+Undead
+Saints
+Saints
+null
+Saints
+Deities
+Deities
+Saints
+Witch
+Okashira
+God Warriors
+God Warriors
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints
+Saints | Dryad
+Saints(former) | Ressurected saint
+Saints
+Saints
+Saints
+null
+Deities
+God Warriors
+Deities
+`;
