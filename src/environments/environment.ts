6fd1c67ea7915c44650c0bafae589e04c09f5565
Diego Chagas <drochachagas@gmail.com>
2019-10-09 21:58:37 -0300

    Change api url to Heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  //apiRest: 'https://saint-seiya-api.herokuapp.com/api'
-  apiRest: 'http://localhost:8080/api'
+  apiRest: 'https://saint-seiya-api.herokuapp.com/api'
+  //apiRest: 'http://localhost:8080/api'
 };
