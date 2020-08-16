fb23915a8517f886e5de5cad578b154f58acadbe
diegochagas <drochachagas@gmail.com>
2020-08-16 09:13:43 -0300

    Add a carousel for the characters view images with all cloth images

--- frontend/src/app/characters/index.ts
+++ frontend/src/app/characters/index.ts
@@ -2,5 +2,6 @@ export * from './character-view';
 export * from './characters-list';
 export * from './curiosities';
 export * from './saints-list';
+export * from './separated-by';
 
 export * from './characters.component';
