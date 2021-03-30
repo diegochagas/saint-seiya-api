244cfef48b16c49ba1a94eed2c1f3306d0e20629
Diego Chagas <drochachagas@gmail.com>
2021-03-30 11:19:49 -0300

    Update saints

--- frontend/src/app/characters/saints-list/saints-list.component.ts
+++ frontend/src/app/characters/saints-list/saints-list.component.ts
@@ -82,11 +82,11 @@ export class SaintsListComponent implements OnInit {
         });
 
         this.groups.push({ title: "Gods", items: response.gods });
-        this.groups.push({ title: "Hades Representative", items: response.representative });
+        this.groups.push({ title: "Representative", items: response.representative });
         this.groups.push({ title: `108 Hades Specters (${unknownSpectersCounter} appeared)`, items: response.evilStars });
-        this.groups.push({ title: "Skeleton soldiers", items: response.skeletons });
+        this.groups.push({ title: "Soldiers", items: response.skeletons });
         this.groups.push({ title: "Other Specters", items: response.otherSpecters });
-        this.groups.push({ title: "Pluto Faceless", items: response.faceless });
+        this.groups.push({ title: "Faceless", items: response.faceless });
       } else if (this.className === 'pallasites') {
         this.groups = [];
 
